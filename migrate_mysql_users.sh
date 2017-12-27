7d3a255e05e57fdcb917813c895b2d37a28d71f3
Cloud User <centos@grauchopf.mteo.prod>
2017-12-27 11:00:54 +0000

    added HOST

--- migrate_mysql_users.sh
+++ migrate_mysql_users.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 user=$1;
 password=$2;
+host="localhost";
+if [ -n "$3" ]
+ then host=$3
+fi
 if [ -z "$1" ] 
  then
- echo "Please use $0 USERNAME PASSWORD" 
+ echo "Please use $0 USERNAME PASSWORD DBHOST(default localhost)" 
  exit 0;
 fi
-mysql -B -N -u$user -p -e "SELECT CONCAT('\'', user,'\'@\'', host, '\'') FROM user WHERE user != 'root' AND user != ''" mysql > mysql_all_users
-while read line; do mysql -B -N -u$user -p$password -e "SHOW GRANTS FOR $line"; done < mysql_all_users > mysql_all_users_sql.sql
+mysql -B -N -u$user -p$password -h$host -e "SELECT CONCAT('\'', user,'\'@\'', host, '\'') FROM user WHERE user != 'root' AND user != ''" mysql > mysql_all_users
+while read line; do mysql -B -N -u$user -p$password -h$host -e "SHOW GRANTS FOR $line"; done < mysql_all_users > mysql_all_users_sql.sql
 sed -i 's/$/;/' mysql_all_users_sql.sql
