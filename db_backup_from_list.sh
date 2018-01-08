ec826c6e17b8192ede1bd24832c725db59c8d740
Alex M <merguriev@mail.ru>
2018-01-08 18:50:54 +0300

    added backup/restore mysql databases from filelist

---
+++ db_backup_from_list.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Please enter root password for mysql:"
+read password
+[ ! -d "db_backup" ] && mkdir db_backup
+cd db_backup
+for db in $(cat dbs)
+do
+ echo "Bachup database: $db" 
+mysqldump -uroot -p$password $db > "$db.sql"
+done
+
---
+++ db_resore_from_list.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo -n "Enter mysql HOSTNAME and press [ENTER]:"
+read  host
+echo -n "Enter root password for mysql and press [ENTER]:"
+read  password
+for db in $(cat dbs)
+do
+ echo "Restore database: $db" 
+mysql -uroot -p$password -h$host -e "create database $db;"
+mysql -uroot -p$password -h$host $db < "$db.sql"
+done
+
